a91a398dcb4d7838a5721e5184260387ffef2d6e
Christianbv <christianbvolz@gmail.com>
2021-07-28 16:05:22 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const obj = {
+    feedback: () => 'Eita pessoa boa!',
+  };
+  obj.name = nome;
+  return obj;
+};
 
 module.exports = createStudent;
